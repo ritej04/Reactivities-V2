77bd3068e56f3fbbbc5d4d05784ddaf2019a4afb
Ritej Aissaoui <aissaouiritej@gmail.com>
2026-06-25 12:15:17 +0100

    Initial commit

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,8 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+namespace Persistence;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Activity> Activities { get; set; } 
+}
